6004d34ed569fad785487d6b9674a493cfbec726
Ruben Norori <kachomanic@hotmail.com>
2019-04-02 21:54:53 -0600

    news

--- views/modules/ingresar.php
+++ views/modules/ingresar.php
@@ -1,5 +1,6 @@
 <h1>INGRESAR:</h1>
 <!-- COMENTARIO NUEVA RAMA: -->
+<!-- THIS IS NEW -->
 	<form method="post" action="">
 		
 		<input type="text" placeholder="Usuario" name="usuarioIngreso" required>
