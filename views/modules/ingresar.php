9e3b3e20f090644c233d31a08a6fe44d2f6f161d
Ruben Norori <kachomanic@hotmail.com>
2019-04-02 21:43:32 -0600

    nueva rama

--- views/modules/ingresar.php
+++ views/modules/ingresar.php
@@ -1,5 +1,5 @@
 <h1>INGRESAR:</h1>
-<!-- COMENTARIO -->
+<!-- COMENTARIO NUEVA RAMA: -->
 	<form method="post" action="">
 		
 		<input type="text" placeholder="Usuario" name="usuarioIngreso" required>
